819feb3b8d64594c8ec5583469ce02c1185df330
unknown <pughbl@oregonstate.edu>
2024-04-10 20:07:50 -0700

    logic implemented for GET /businesses

--- server.js
+++ server.js
@@ -7,10 +7,45 @@ app.use(express.json());
 // let current_message_index = 0;
 // let messages = {};
 
+let businesses = {};
+let reviews = {};
+let photos = {};
+
 // // Start listening on that port for connections
 app.listen(process.env.PORT, () => {
     console.log("Server ready!");
 });
+
+///////////////////////////////////////////////////////////// BUSINESSES
+app.get('/businesses', (req, res, next) => {
+    var page = parseInt(req.query.page) || 1;
+    var numPerPage = 5;
+    var lastPage = Math.ceil(lodgings.length / numPerPage);
+    page = page < 1 ? 1 : page;
+    page = page > lastPage ? lastPage : page;
+    var start = (page - 1) * numPerPage;
+    var end = start + numPerPage;
+    var pageBusinesses = businesses.slice(start, end);
+    var links = {};
+    if (page < lastPage) {
+        links.nextPage = '/businesses?page=' + (page + 1);
+        links.lastPage = '/businesses?page=' + lastPage;
+    }
+    if (page > 1) {
+        links.prevPage = '/businesses?page=' + (page - 1);
+        links.firstPage = '/businesses?page=1';
+    }
+    res.status(200).json({
+        pageNumber: page,
+        totalPages: lastPage,
+        pageSize: numPerPage,
+        totalCount: businesses.length,
+        businesses: pageBusinesses,
+        links: links
+    });
+    res.send(businesses);
+});
+/////////////////////////////////////////////////////////////
 
 // //Handle certain API endpoints
 // app.get("/messages", (req, res, next) => {
