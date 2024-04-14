2a4bff6ec150af86d8f8ce892e808292939744ad
unknown <pughbl@oregonstate.edu>
2024-04-13 21:52:11 -0700

    Last bit of changes before project 1 check in 2 submission

--- server.js
+++ server.js
@@ -28,10 +28,8 @@ app.listen(process.env.PORT, () => {
 function post_verify(body, req_fields, opt_fields) {
     // user passes in object
     // object should be iterated over, and each key of object should be checked to make sure it exists in required or optional fields
-    
     // object should be checked to make sure it has ALL required fields
 
-    // iterate over required fields
     body_keys = Object.keys(body);
     if (body_keys.length < req_fields.length || body_keys.length > (req_fields.length + opt_fields.length)) {
         return false;
@@ -97,6 +95,15 @@ app.get('/businesses', (req, res, next) => {
     });
 });
 
+app.get("/businesses/:businessID", (req, res, next) => {
+    var businessID = parseInt(req.params.businessID);
+    if (businesses[businessID]) {
+        res.status(200).json({business: businesses[businessID], reviews: reviews, photos: photos});
+    } else {
+        next();
+    };
+});
+
 app.post("/businesses", (req, res) => {
     if (post_verify(req.body, required_business_fields, optional_business_fields) == true) {
         business_index++;
@@ -117,15 +124,6 @@ app.post("/businesses", (req, res) => {
             response_text = "Error: Missing field(s)";
         }
         res.send(response_text);
-    };
-});
-
-app.get("/businesses/:businessID", (req, res, next) => {
-    var businessID = parseInt(req.params.businessID);
-    if (businesses[businessID]) {
-        res.status(200).json(businesses[businessID]);
-    } else {
-        next();
     };
 });
 
@@ -193,12 +191,17 @@ app.get('/businesses/:businessID/reviews', (req, res, next) => {
     });
 });
 
+app.get('/users/:userID/reviews', (req, res, next) => {
+    var userID = parseInt(req.params.userID);
+    res.status(200).send(reviews)
+});
+
 app.post("/businesses/:businessID/reviews", (req, res, next) => {
+    // Note that it will eventually need to be limited to one review per user
     if (post_verify(req.body, required_review_fields, optional_review_fields) == true) {
         review_index++;
     
         reviews[review_index] = req.body;
-        // DOES THIS WORK!?!?!?? WTF
         req.body.links = {"review": `/businesses/${req.params.businessID}/reviews/${review_index}`};
     
         res.status(201);
@@ -212,7 +215,6 @@ app.post("/businesses/:businessID/reviews", (req, res, next) => {
         res.send(response_text);
     }
 });
-
 
 app.put("/businesses/:businessID/reviews/:reviewID", (req, res, next) => {
     var businessID = parseInt(req.params.businessID);
@@ -257,13 +259,67 @@ app.delete('/businesses/:businessID/reviews/:reviewID', (req, res, next) => {
     // };
 });
 
-app.get('/users/:userID/reviews', (req, res, next) => {
-    var userID = parseInt(req.params.userID);
-    res.status(200).send(reviews)
-});
 ////////////////////////////////////////////////////////////////////////////////////////
 
 //////////////////////////////////////////////////////////////////////////////////////// PHOTOS
+
+app.get('/users/:userID/businesses', (req, res, next) => {
+    userID = parseInt(req.query.userID);
+    var page = parseInt(req.query.page) || 1;
+    var numPerPage = 5;
+    var lastPage = Math.ceil(businesses.length / numPerPage);
+    page = page < 1 ? 1 : page;
+    page = page > lastPage ? lastPage : page;
+    var start = (page - 1) * numPerPage;
+    var end = start + numPerPage;
+    var pageBusinesses = Object.entries(businesses).slice(start, end);
+    var links = {};
+    if (page < lastPage) {
+        links.nextPage = `/users/${userID}/businesses?page=` + (page + 1);
+        links.lastPage = `/users/${userId}/businesses?page=` + lastPage;
+    };
+    if (page > 1) {
+        links.prevPage = `/users/${userID}/businesses?page=` + (page - 1);
+        links.firstPage = `/users/${userID}/businesses?page=1`;
+    };
+    res.status(200).json({
+        pageNumber: page,
+        totalPages: lastPage,
+        pageSize: numPerPage,
+        totalCount: businesses.length,
+        businesses: pageBusinesses,
+        links: links
+    });
+});
+
+app.get('/users/:userID/photos', (req, res, next) => {
+    var userID = parseInt(req.query.userID);
+    var page = parseInt(req.query.page) || 1;
+    var numPerPage = 5;
+    var lastPage = Math.ceil(photos.length / numPerPage);
+    page = page < 1 ? 1 : page;
+    page = page > lastPage ? lastPage : page;
+    var start = (page - 1) * numPerPage;
+    var end = start + numPerPage;
+    var pagePhotos = Object.entries(photos).slice(start, end);
+    var links = {};
+    if (page < lastPage) {
+        links.nextPage = `/users/${userID}/photos?page=` + (page + 1);
+        links.lastPage = `/users/${userID}/photos?page=` + lastPage;
+    };
+    if (page > 1) {
+        links.prevPage = `/users/${userID}/photos?page=` + (page - 1);
+        links.firstPage = `/users/${userID}/photos?page=1`;
+    };
+    res.status(200).json({
+        pageNumber: page,
+        totalPages: lastPage,
+        pageSize: numPerPage,
+        totalCount: photos.length,
+        photos: pagePhotos,
+        links: links
+    });
+});
 
 app.post("/businesses/:businessID/photos", (req, res, next) => {
     if (post_verify(req.body, required_photo_fields, optional_photo_fields) == true) {
@@ -320,64 +376,6 @@ app.put("/users/:userID/photos/:photoID", (req, res, next) => {
     } else {
         next();
     };
-});
-
-app.get('/users/:userID/photos', (req, res, next) => {
-    var userID = parseInt(req.query.userID);
-    var page = parseInt(req.query.page) || 1;
-    var numPerPage = 5;
-    var lastPage = Math.ceil(photos.length / numPerPage);
-    page = page < 1 ? 1 : page;
-    page = page > lastPage ? lastPage : page;
-    var start = (page - 1) * numPerPage;
-    var end = start + numPerPage;
-    var pagePhotos = Object.entries(photos).slice(start, end);
-    var links = {};
-    if (page < lastPage) {
-        links.nextPage = `/users/${userID}/photos?page=` + (page + 1);
-        links.lastPage = `/users/${userID}/photos?page=` + lastPage;
-    };
-    if (page > 1) {
-        links.prevPage = `/users/${userID}/photos?page=` + (page - 1);
-        links.firstPage = `/users/${userID}/photos?page=1`;
-    };
-    res.status(200).json({
-        pageNumber: page,
-        totalPages: lastPage,
-        pageSize: numPerPage,
-        totalCount: photos.length,
-        photos: pagePhotos,
-        links: links
-    });
-});
-
-app.get('/users/:userID/businesses', (req, res, next) => {
-    userID = parseInt(req.query.userID);
-    var page = parseInt(req.query.page) || 1;
-    var numPerPage = 5;
-    var lastPage = Math.ceil(businesses.length / numPerPage);
-    page = page < 1 ? 1 : page;
-    page = page > lastPage ? lastPage : page;
-    var start = (page - 1) * numPerPage;
-    var end = start + numPerPage;
-    var pageBusinesses = Object.entries(businesses).slice(start, end);
-    var links = {};
-    if (page < lastPage) {
-        links.nextPage = `/users/${userID}/businesses?page=` + (page + 1);
-        links.lastPage = `/users/${userId}/businesses?page=` + lastPage;
-    };
-    if (page > 1) {
-        links.prevPage = `/users/${userID}/businesses?page=` + (page - 1);
-        links.firstPage = `/users/${userID}/businesses?page=1`;
-    };
-    res.status(200).json({
-        pageNumber: page,
-        totalPages: lastPage,
-        pageSize: numPerPage,
-        totalCount: businesses.length,
-        businesses: pageBusinesses,
-        links: links
-    });
 });
 
 ////////////////////////////////////////////////////////////////////////////////////////
